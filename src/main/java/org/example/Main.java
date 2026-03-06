93832e89f66033926de117cadf8bfce8fccf8951
Dario105 <darioabednego@gmail.com>
2026-03-06 17:36:43 -0600

    haciendo comit con mi otra rama

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("HOLA MUNDO ESTE ES MI PROYECTO EN GIT");
+        System.out.println("Este es el cambio de mi rama development");
     }
 }
